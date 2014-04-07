08fd33a3d24d6930b5d08f2bd31b262e7d99f500
Ashley Luhrs <aluhrs@gmail.com>
2014-04-07 14:40:36 -0700

    updating templates/map.html

--- places.py
+++ places.py
@@ -6,7 +6,6 @@ import urllib2
 import json
 import config
 
-#test with limit 1
 points = model.session.query(model.Crowd_Sourced).all()
 
 # encode the url
@@ -17,46 +16,40 @@ def get_location(points):
 			if len(locations_list) < 995:
 				if points[i].grocery_reason == None and points[i].transportation_reason == None and points[i].food_reason == None and points[i].other_poi_reason == None:
 					id = points[i].id
-					#print id
 					curr = model.session.query(model.Crowd_Sourced).filter_by(id=id).one()
 					latitude = points[i].latitude
 					longitude = points[i].longitude				
 					url = build_url(latitude, longitude)
 					json_data = get_data(url)
 					
-					# print json_data
 					for e in range(len(json_data["results"])):
 						types = json_data["results"][e]["types"]
-						# types_len = len(types)
-						# print types_len
 						for i in range(len(types)):
-							# if i == types_len:
 							if types[i] == "grocery_or_supermarket":
 								#print "For %r: For groceries: These are the types: %r. This is the number in the list: %r. This is what I am adding: %r" % (id, types, i, types[i])
 								curr.grocery_reason = True
 								model.session.add(curr)
-								model.session.commit()
+								#model.session.commit()
 							if types[i] in ["bus_station", "subway_station", "train_station"]:
 								#print "For %r: For transportation: These are the types: %r. This is the number in the list: %r. This is what I am adding: %r" % (id, types, i, types[i])
 								curr.transportation_reason = True
 								model.session.add(curr)
-								model.session.commit()
+								#model.session.commit()
 							if types[i] == "food":
 								#print "For %r: For food: These are the types: %r. This is the number in the list: %r. This is what I am adding: %r" % (id, types, i, types[i])
 								curr.food_reason = True
 								model.session.add(curr)
-								model.session.commit()
+								#model.session.commit()
 							if types[i] in ["home_goods_store", "movie_theater", "park", "shopping_mall"]:
 								#print "For %r: For other: These are the types: %r. This is the number in the list: %r. This is what I am adding: %r" % (id, types, i, types[i])
 								curr.other_poi_reason = True
 								model.session.add(curr)
-								model.session.commit()
+								#model.session.commit()
 					locations_list.append(id)			
-	#create_file(locations_list)				
-	#print locations_list
+
 	print len(locations_list)
 			
-	#model.session.commit()
+	model.session.commit()
 	print "The points of interest information have been added to the database"
 
 # use urllib2 to open the url
@@ -78,18 +71,9 @@ def create_file(data):
 
 	print "Your file places.txt has been created."
 
-# def parse(json_data):
-# 	# for loop to go through length of the list of types
-
-# 	for e in range(len(json_data["results"])):
-# 		types = json_data["results"][e]["types"]
-# 		for i in range(len(types)):
-# 			if types[i] == "neighborhood":
-# 				print types[i]
-
-# 	return json_data["results"][0]["types"]
-
 """
+Example of json from Google Maps Api Places
+
  {
          "geometry" : {
             "location" : {
@@ -117,20 +101,5 @@ def create_file(data):
 
 """
 
-"""
-for e in range(len(json_data["results"])):
-	print json_data["results"][e]
-
-for i in range(len(types)):
-...     if types[i] == "neighborhood":
-...             print types[i]
-
->>> for e in range(len(json_data["results"])):
-...     types = json_data["results"][e]["types"]
-...     for i in range(len(types)):
-...             if types[i] == "neighborhood":
-...                     print types[i]
-
-"""
 if __name__ == "__main__":
 	url = get_location(points)
